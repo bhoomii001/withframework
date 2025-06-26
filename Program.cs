f0bdd99ed9a8425536d704254b39aa61707be23a
Bhoomi <bhoomiahir321@gmail.conm>
2025-06-26 15:53:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace withframework
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+
+            Console.Read();
+        }
+    }
+}
---
+++ sum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace withframework
+{
+    internal class sum
+    {
+        public static void Main(String[] args)
+        {
+            //int a = 5;
+            //int b = 6;
+            //int sum;
+            //sum = a + b;
+            //Console.WriteLine(sum);
+            //Console.Read();
+
+            //   int a = int.Parse(Console.Read());
+
+
+            int a = Convert.ToInt32(Console.ReadLine());
+         
+            int b = Convert.ToInt32(Console.ReadLine());
+          
+            int sum = a + b;
+            Console.Write("sum of two number "+ sum);
+
+            Console.ReadLine();
+        }
+    }
+}
